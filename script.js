f4a4ab6c652a796e2202dbc0dfbc5413428e0454
me-techshruti54212 <shrutiverma.me@gmail.com>
2023-10-29 21:52:10 +0530

    added files

---
+++ script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+  autoplay: {
+    delay: 7000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
